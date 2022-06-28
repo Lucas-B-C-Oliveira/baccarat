0f8b1e4ba2787698c6c6eccc88858ad65b44ed4d
Lucas Barbosa Cesar de Oliveira <lucascoliveira819@gmail.com>
2022-06-28 10:50:16 -0400

    50% Implemented render balls functionality on the bottom screen

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,30 @@ import { useRef, useState } from 'react'
 
 function App() {
 
-  const [cellsOfUpInGame, setCellsOfUpInGame] = useState([])
+  const [cellsOfTopInGame, setCellsOfTopInGame] = useState([])
+  const [cellsOfBottomInGame, setCellsOfBottomInGame] = useState([])
   const currentMatch = useRef(0)
-  const columnOfUp = useRef(0)
-  const lineOfUp = useRef(0)
+  const columnOfTop = useRef(0)
+  const lineOfTop = useRef(0)
 
-  const X_AND_Y_POS_MULTIPLIER_OF_UP_CELL = 3.25
-  const Y_STARTING_POS_OF_UP_CELL = 1.9
-  const X_STARTING_POS_OF_UP_CELL = 2.2
+  const lineOfBottom = useRef(0)
+  const columnOfBottom = useRef(0)
+
+  const lastLockedLineOfBottom = useRef(7)
+  const oldBallOfBottomCell = useRef(0)
+  const oldCorrectColumnOfBottomCell = useRef(0)
+  const oldCorrectLineOfBottomCell = useRef(0)
+  const lastLockedColumnOfBottom = useRef(0)
+  const initialLineFree = useRef(0)
+  const wasInALongSequence = useRef(false)
+  const lastFreeLineOfLockedColumn = useRef(false)
+
+  const X_AND_Y_POS_MULTIPLIER_OF_TOP_CELL = 3.25
+
+  const Y_STARTING_POS_OF_TOP_CELL = 1.9
+  const Y_STARTING_POS_OF_BOTTOM_CELL = 22.18
+
+  const X_STARTING_POS_OF_CELL = 2.2
 
   const updateCurrentMatch = (newImage) => {
 
@@ -19,45 +35,158 @@ function App() {
 
     if (currentMatch.current === 0) {
 
-      const posOfUpCell = {
-        x: `${X_STARTING_POS_OF_UP_CELL}rem`,
-        y: `${Y_STARTING_POS_OF_UP_CELL}rem`
+      /// ##### Top Cells
+
+      const posOfTopCell = {
+        x: `${X_STARTING_POS_OF_CELL}rem`,
+        y: `${Y_STARTING_POS_OF_TOP_CELL}rem`
       }
 
-      const newCellOfUp = {
+      const newCellOfTop = {
         key: currentMatch.current,
-        position: posOfUpCell,
+        position: posOfTopCell,
         image: newImage,
       }
 
-      setCellsOfUpInGame(prevState => [...prevState, newCellOfUp])
+      /// ##### Bottom Cells
+
+      const posOfBottomCell = {
+        x: `${X_STARTING_POS_OF_CELL}rem`,
+        y: `${Y_STARTING_POS_OF_BOTTOM_CELL}rem`
+      }
+
+      const newCellOfBottom = {
+        key: currentMatch.current,
+        position: posOfBottomCell,
+        image: newImage,
+      }
+
+      setCellsOfBottomInGame(prevState => [...prevState, newCellOfBottom])
+      setCellsOfTopInGame(prevState => [...prevState, newCellOfTop])
+      oldBallOfBottomCell.current = newImage
 
     }
     else {
 
-      lineOfUp.current = lineOfUp.current + 1
+      lineOfTop.current = lineOfTop.current + 1
 
       if (currentMatch.current % 6 === 0) {
-        columnOfUp.current = columnOfUp.current + 1
-        lineOfUp.current = 0
+        columnOfTop.current = columnOfTop.current + 1
+        lineOfTop.current = 0
 
       }
 
-      const newYPosOfUpCell = Y_STARTING_POS_OF_UP_CELL + (X_AND_Y_POS_MULTIPLIER_OF_UP_CELL * lineOfUp.current)
-      const newXPosOfUpCell = X_STARTING_POS_OF_UP_CELL + (X_AND_Y_POS_MULTIPLIER_OF_UP_CELL * columnOfUp.current)
+      const newYPosOfTopCell = Y_STARTING_POS_OF_TOP_CELL + (X_AND_Y_POS_MULTIPLIER_OF_TOP_CELL * lineOfTop.current)
+      const newXPosOfTopCell = X_STARTING_POS_OF_CELL + (X_AND_Y_POS_MULTIPLIER_OF_TOP_CELL * columnOfTop.current)
 
-      const posOfUpCell = {
-        x: `${newXPosOfUpCell}rem`,
-        y: `${newYPosOfUpCell}rem`
+      const posOfTopCell = {
+        x: `${newXPosOfTopCell}rem`,
+        y: `${newYPosOfTopCell}rem`
       }
 
-      const newCellOfUp = {
+      const newCellOfTop = {
         key: currentMatch.current,
-        position: posOfUpCell,
+        position: posOfTopCell,
         image: newImage,
       }
 
-      setCellsOfUpInGame(prevState => [...prevState, newCellOfUp])
+      /// ##### Bottom Cells
+
+      let newYPosOfBottomCell = 0
+      let newXPosOfBottomCell = 0
+
+
+
+      if (oldBallOfBottomCell.current === newImage) {
+
+        /// #### Put the ball down
+
+
+        if (lineOfBottom.current === lastLockedLineOfBottom.current) {
+
+          columnOfBottom.current = columnOfBottom.current + 1
+
+          newYPosOfBottomCell = Y_STARTING_POS_OF_BOTTOM_CELL + (X_AND_Y_POS_MULTIPLIER_OF_TOP_CELL * lastLockedLineOfBottom.current)
+          newXPosOfBottomCell = X_STARTING_POS_OF_CELL + (X_AND_Y_POS_MULTIPLIER_OF_TOP_CELL * columnOfBottom.current)
+
+          lastLockedColumnOfBottom.current = columnOfBottom.current
+          lastFreeLineOfLockedColumn.current = true
+
+
+
+
+        }
+        else {
+
+          lineOfBottom.current = lineOfBottom.current + 1
+          oldCorrectColumnOfBottomCell.current = columnOfBottom.current
+
+          wasInALongSequence.current = true
+          lastLockedColumnOfBottom.current = columnOfBottom.current
+          oldCorrectLineOfBottomCell.current = lineOfBottom.current
+
+          newYPosOfBottomCell = Y_STARTING_POS_OF_BOTTOM_CELL + (X_AND_Y_POS_MULTIPLIER_OF_TOP_CELL * lineOfBottom.current)
+          newXPosOfBottomCell = X_STARTING_POS_OF_CELL + (X_AND_Y_POS_MULTIPLIER_OF_TOP_CELL * columnOfBottom.current)
+        }
+
+      }
+      else {
+
+        lineOfBottom.current = initialLineFree.current
+
+        if (wasInALongSequence.current) {
+          columnOfBottom.current = oldCorrectColumnOfBottomCell.current
+          wasInALongSequence.current = false
+        }
+
+        if (lastFreeLineOfLockedColumn.current) {
+          lastLockedLineOfBottom.current = lastLockedLineOfBottom.current - 1
+          lastFreeLineOfLockedColumn.current = false
+
+        }
+
+        if (columnOfBottom.current >= 35) {
+          initialLineFree.current = initialLineFree.current + 1
+          columnOfBottom.current = lastLockedColumnOfBottom.current
+
+
+          if (oldCorrectLineOfBottomCell.current < (initialLineFree.current + 1)) {
+
+            lastLockedColumnOfBottom.current = -1
+          }
+
+        }
+
+        console.log(columnOfBottom.current)
+
+        // if (columnOfBottom.current + 1 >= 35) {
+        //   columnOfBottom.current = -1
+        // }
+
+        columnOfBottom.current = columnOfBottom.current + 1
+
+        newYPosOfBottomCell = Y_STARTING_POS_OF_BOTTOM_CELL + (X_AND_Y_POS_MULTIPLIER_OF_TOP_CELL * initialLineFree.current)
+        newXPosOfBottomCell = X_STARTING_POS_OF_CELL + (X_AND_Y_POS_MULTIPLIER_OF_TOP_CELL * columnOfBottom.current)
+
+      }
+
+      const posOfBottomCell = {
+        x: `${newXPosOfBottomCell}rem`,
+        y: `${newYPosOfBottomCell}rem`
+      }
+
+      const newCellOfBottom = {
+        key: currentMatch.current,
+        position: posOfBottomCell,
+        image: newImage,
+      }
+
+
+      //////_____________________
+      oldBallOfBottomCell.current = newImage
+      setCellsOfTopInGame(prevState => [...prevState, newCellOfTop])
+      setCellsOfBottomInGame(prevState => [...prevState, newCellOfBottom])
+
 
     }
 
@@ -67,14 +196,14 @@ function App() {
 
   return (
     <>
-      <Game cellsOfUpInGame={cellsOfUpInGame} />
+      <Game cellsOfTopInGame={cellsOfTopInGame} cellsOfBottomInGame={cellsOfBottomInGame} />
       <Control updateCurrentMatch={updateCurrentMatch} />
       <button
         onClick={() => {
-          setCellsOfUpInGame([])
+          setCellsOfTopInGame([])
           currentMatch.current = 0
-          columnOfUp.current = 0
-          lineOfUp.current = 0
+          columnOfTop.current = 0
+          lineOfTop.current = 0
         }}
         type="button"
         className="tw-btn-red"
--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -4,17 +4,23 @@ import Cell from './Cell'
 // import Player2 from '../../assets/red-ball.png' //! For Teste - Delete this
 // import Player3 from '../../assets/green-ball.png' //! For Teste - Delete this
 
-
-function Game({ cellsOfUpInGame }) {
+function Game({ cellsOfTopInGame, cellsOfBottomInGame }) {
   return (
     <div className="h-[1080px] w-[1920px] bg-main">
       {/* <div className="min-h-screen min-w-screen bg-main bg-size-cover bg-left-top"> */}
 
       {/* <img src={Player2} className="absolute top-[1.9rem] left-[24.95rem] h-[2.5rem] w-[2.5rem]"></img> */}
       {/* <img src={Player2} className="absolute top-[11.6rem] left-[2.2rem] h-[2.5rem] w-[2.5rem]"></img> */}
-      {cellsOfUpInGame.map(cel => (
+
+
+      {cellsOfTopInGame.map(cel => (
         <Cell key={cel.key} position={cel.position} image={cel.image} />)
       )}
+
+      {cellsOfBottomInGame.map(cel => (
+        <Cell key={cel.key} position={cel.position} image={cel.image} />)
+      )}
+
     </div>
 
   )
